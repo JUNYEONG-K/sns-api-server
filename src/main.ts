bf2ac3688dc06dec76e0ed771c4b2c3c963ae3b1
JUNYEONG-K <godoly1211@naver.com>
2024-04-04 23:04:02 +0900

    feat. API 응답 - @Expose 필드만 반환하도록

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { swaggerConfig } from './swagger.config';
+import { ClassSerializerInterceptor } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalInterceptors(
+    new ClassSerializerInterceptor(app.get(Reflector), {
+      strategy: 'excludeAll',
+    }),
+  );
 
   swaggerConfig(app);
 
--- src/users/dto/response/user.dto.ts
+++ src/users/dto/response/user.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Expose } from 'class-transformer';
 
 export class UserDto {
   @ApiProperty()
+  @Expose()
   nickname: string;
 
   @ApiProperty()
+  @Expose()
   email: string;
 }
