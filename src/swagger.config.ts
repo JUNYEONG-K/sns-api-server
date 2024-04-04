d512ca9b9b466cff3fb887c32ef2f34ae870eeaf
JUNYEONG-K <godoly1211@naver.com>
2024-04-04 20:00:07 +0900

    feat. 스웨거 설정

---
+++ src/swagger.config.ts
@@ -0,0 +1,23 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+const versions = ['0.0.1'];
+
+const info = {
+  title: 'SNS API',
+  version: versions[versions.length - 1],
+  description: 'SNS 앱 API 서버 문서입니다.',
+};
+
+export function swaggerConfig(app: INestApplication): void {
+  const documentBuilder = new DocumentBuilder();
+
+  const options = documentBuilder
+    .setTitle(info.title)
+    .setDescription(info.description)
+    .setVersion(info.version)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+}
