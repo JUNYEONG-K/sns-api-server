31fc1013d23504de113c4abd536d28e3c2229d52
JUNYEONG-K <godoly1211@naver.com>
2024-04-04 23:33:12 +0900

    feat. config 디렉토리

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { swaggerConfig } from './swagger.config';
+import { swaggerConfig } from './config/swagger.config';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
