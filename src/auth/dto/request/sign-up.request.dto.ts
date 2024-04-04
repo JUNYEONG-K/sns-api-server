15d85392108018664060cd8f2a6261fd7abe3c6b
JUNYEONG-K <godoly1211@naver.com>
2024-04-04 23:06:18 +0900

    feat. API 요청 - 요청 필드들 검증하도록

--- src/auth/dto/request/sign-up.request.dto.ts
+++ src/auth/dto/request/sign-up.request.dto.ts
@@ -1,12 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class SignUpRequestDto {
   @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
   nickname: string;
 
   @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
   email: string;
 
   @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
   password: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { swaggerConfig } from './swagger.config';
-import { ClassSerializerInterceptor } from '@nestjs/common';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
   app.useGlobalInterceptors(
     new ClassSerializerInterceptor(app.get(Reflector), {
       strategy: 'excludeAll',
