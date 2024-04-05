2e1d004503028b0695f603553df21d8e5d721e95
JUNYEONG-K <godoly1211@naver.com>
2024-04-06 02:13:07 +0900

    feat. 주석

--- src/feeds/dto/request/upload-feed.request.dto.ts
+++ src/feeds/dto/request/upload-feed.request.dto.ts
@@ -9,5 +9,5 @@ export class UploadFeedRequestDto {
 
   @ApiProperty({ type: [Number] })
   @IsNotEmpty()
-  hashtagIds: number[];
+  hashtagIds: number[]; //TODO: hashtags: string[] 으로 받아서 서버에서 처리해야할 것 같은 느낌?
 }
