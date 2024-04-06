789b4db16d27b4b1ee9d3ba4a5d13d58f9af52b8
JUNYEONG-K <godoly1211@naver.com>
2024-04-07 00:45:12 +0900

    feat. 좋아요 컨트롤러, 서비스 추가

--- src/like/like.module.ts
+++ src/like/like.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { FeedsLikeService } from './feeds-like.service';
+import { LikeController } from './like.controller';
+import { CommentsLikeService } from './comments-like.service';
 
 @Module({
   imports: [PrismaModule],
-  providers: [FeedsLikeService],
+  controllers: [LikeController],
+  providers: [FeedsLikeService, CommentsLikeService],
 })
 export class LikeModule {}
